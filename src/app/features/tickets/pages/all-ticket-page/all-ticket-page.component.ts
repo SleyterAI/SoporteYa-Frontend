9e74798ef4f631f8c6de069c963a17bbbe0148b9
Sleyter Astete Ibañez <146astt@gmail.com>
2026-09-17 10:58:21 -0500

    6th cmmt: refactor: filters updated now works¡

--- src/app/features/tickets/pages/all-ticket-page/all-ticket-page.component.ts
+++ src/app/features/tickets/pages/all-ticket-page/all-ticket-page.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { TicketService } from '../../services/ticket.service';
 import { NgClass } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { TimeAgoPipe } from '../../../../core/pipes/time-ago.pipe';
+import { CategoriaService } from '../../services/categoria.service';
 
 @Component({
   selector: 'app-all-ticket-page',
@@ -13,12 +14,36 @@ import { TimeAgoPipe } from '../../../../core/pipes/time-ago.pipe';
 })
 export class AllTicketPageComponent {
   private readonly ticketService = inject(TicketService);
+  private readonly categoriaService = inject(CategoriaService);
+
+  selectedEstado = signal<string>('');
+  selectedPrioridad = signal<string>('');
 
   readonly ticketResource = rxResource({
-    stream: () => this.ticketService.getTickets()
+    stream: () => {
+      const estado = this.selectedEstado() || undefined;
+      const prioridad = this.selectedPrioridad() || undefined;
+
+      return this.ticketService.getTickets(estado, prioridad);
+    }
   });
 
-  // Helper para crear el círculo con las iniciales del usuario
+  readonly categoriaResource = rxResource({
+    stream: () => this.categoriaService.getAllCategoria()
+  });
+
+  onEstadoChange(event: Event) {
+    const value = (event.target as HTMLSelectElement).value;
+    this.selectedEstado.set(value);
+    this.ticketResource.reload();
+  }
+
+  onPrioridadChange(event: Event) {
+    const value = (event.target as HTMLSelectElement).value;
+    this.selectedPrioridad.set(value);
+    this.ticketResource.reload();
+  }
+
   getInitials(name: string): string {
     if (!name) return 'U';
     const parts = name.split(' ');
