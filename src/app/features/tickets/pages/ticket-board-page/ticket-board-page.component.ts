df944fe4a561bf739d1ea773d12c7d0371190044
Sleyter Astete Ibañez <146astt@gmail.com>
2026-09-21 23:11:05 -0500

    14th cmmt: refactor: ticket-board-page card order implemented in columns by status

--- src/app/features/tickets/pages/ticket-board-page/ticket-board-page.component.ts
+++ src/app/features/tickets/pages/ticket-board-page/ticket-board-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { TicketService } from '../../services/ticket.service';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { TicketRowComponent } from '../../components/ticket-row/ticket-row.component';
@@ -18,5 +18,25 @@ export class TicketBoardPageComponent {
 
   readonly ticketResource = rxResource({
     stream: () => this.ticketService.getTickets()
+  });
+
+  ticketsAbiertos = computed(() => {
+    const list = this.ticketResource.value() || [];
+    return list.filter(t => (t.estado || 'ABIERTO') === 'ABIERTO');
+  });
+
+  ticketsEnProgreso = computed(() => {
+    const list = this.ticketResource.value() || [];
+    return list.filter(t => t.estado === 'EN_PROGRESO');
+  });
+
+  ticketsResueltos = computed(() => {
+    const list = this.ticketResource.value() || [];
+    return list.filter(t => t.estado === 'RESUELTO');
+  });
+
+  ticketsCerrados = computed(() => {
+    const list = this.ticketResource.value() || [];
+    return list.filter(t => t.estado === 'CERRADO');
   });
 }
